43cfaa888e467c06c0a97b3f2706a37c29325a1e
Zargad <t0pl0l250@gmail.com>
2023-01-10 19:54:03 +0000

    working tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,80 @@
-use std::collections::HashMap;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+
+enum Keyword {
+    Namespace,
+    Use,
+}
+
+
+#[derive(Debug)]
+enum Token {
+    None,
+    Symbol(char),
+    Identifier(String),
+    Number(String),
+    // Keyword(Keyword),
+}
+impl Token {
+    pub fn vec_from_string(string: String) -> Vec<Self> {
+        let mut result: Vec<Self> = vec![];
+        let mut last: Self = Token::None;
+        let mut is_space = true;
+        for c in string.chars() {
+            if c.is_whitespace() {
+                is_space = true;
+                continue;
+            }
+            if is_space || !last.try_push(c) {
+                if !last.is_none() {
+                    result.push(last);
+                }
+                last = Self::from_char(c);
+            }
+            is_space = false;
+        }
+        result.push(last);
+        return result;
+    }
+    fn is_none(&self) -> bool {
+        if let Self::None = self {
+            return true;
+        }
+        return false;
+    }
+    fn from_char(c: char) -> Self {
+        if c.is_alphabetic() {
+            Token::Identifier(c.to_string())
+        }
+        else if c.is_numeric() {
+            Token::Number(c.to_string())
+        }
+        else {
+            Token::Symbol(c)
+        }
+    }
+    fn try_push(&mut self, c: char) -> bool {
+        let is_matching = match self {
+            Token::Identifier(_) => c.is_alphanumeric(),
+            Token::Number(_) => c.is_numeric(),
+            Token::Symbol(_) | Token::None => false,
+        };
+        if is_matching {
+            match self {
+                Token::Identifier(s) | Token::Number(s) => s.push(c),
+                Token::Symbol(_) | Token::None => (),
+            }
+            return true;
+        }
+        return false;
+    }
+}
+
+
+struct AbstractSyntaxTree {
+    
+}
 
 
 struct GraphingCalculator {
@@ -11,6 +87,15 @@ struct GraphingCalculator {
             api_key: "dcb31709b452b1cf9dc26972add0fda6".to_string(),
         }
     }
+    pub fn from_file(path: &str) -> Self {
+        let file = File::open(path).expect("Well that sucked");
+        let reader = BufReader::new(file);
+        let mut expressions: Vec<String> = vec![];
+        for line in reader.lines() {
+            expressions.push(line.unwrap());
+        }
+        Self::new(expressions)
+    }
     pub fn print_html(&self) {
         println!(r"<script src='{}'></script>
 <div id='calculator' style='width: 600px; height: 400px;'></div>
@@ -22,9 +107,6 @@ struct GraphingCalculator {
         }
         println!("</script>");
     }
-    fn get_style(&self) -> String {
-        format!("width: {}; height: {};", "600px", "400px").to_string()
-    }
     fn get_api_link(&self) -> String {
         format!("https://www.desmos.com/api/v1.7/calculator.js?apiKey={}", self.api_key).to_string()
     }
@@ -32,9 +114,6 @@ struct GraphingCalculator {
 
 
 fn main() {
-    GraphingCalculator::new(vec![
-        "y=x^2".to_string(),
-        "y=2x".to_string(),
-        "y=sin(x)".to_string(),
-    ]).print_html();
+    let bruh = Token::vec_from_string("x = {\n  x=5:1,\n  0\n}".to_string());
+    println!("{:?}", bruh);
 }
